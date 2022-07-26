008af849afab3e67893c05684695ce7bd568e5c6
95choiJH <80238745+95choiJH@users.noreply.github.com>
2022-07-26 20:21:00 +0900

    최종수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,43 @@
 $(function(){
     
+    $( window ).resize( function() {
+        if (matchMedia("screen and (min-width: 1201px)").matches) {
+            $('body').removeClass('none-scroll')
+            $('.open-menu-wrap').addClass('hide');
+        }
+    });
+    
+    $('.gnb-area .ic-menu').click(function(){
+        $(this).siblings('.open-menu-wrap').removeClass('hide');
+        $('body').addClass('none-scroll');
+
+        lol = gsap.timeline({delay: .5})
+        .to($('.lol1'), .3,{ className:"lol1" })
+        .to($('.lol2'), .3,{ className:"lol2" })
+        .to($('.lol3'), .3,{ className:"lol3" })
+        .to($('.lol3'), .3,{ className:"lol3 hide" })
+        .to($('.lol2'), .3,{ className:"lol2 hide" })
+        .to($('.lol1'), .3,{ className:"lol1 hide" })
+        .to($('.lol1'), .3,{ className:"lol1" })
+        .to($('.lol2'), .3,{ className:"lol2" })
+        .to($('.lol3'), .3,{ className:"lol3" })
+        .to($('.lol4'), .3,{ className:"lol4" })
+        .to($('.lol5'), .3,{ className:"lol5" })
+    })
+    $('.header .btn-close').click(function(e){
+        e.preventDefault();
+        $(this).parents('.open-menu-wrap').addClass('hide');
+        $('body').removeClass('none-scroll')
+        lol.kill();
+        $('.ani-lol span').each(function(){
+            $(this).addClass('hide');
+        })
+    }) // 메뉴 닫기 버튼 클릭시 메뉴 영역 닫고 body 스크롤 초기화
     if (matchMedia("screen and (min-width: 1201px)").matches) {
+        $('body').removeClass('none-scroll')
+        $('.ani-lol span').each(function(){
+            $(this).addClass('hide');
+        })
 
         $('.gnb-area .menu-area .menu-item').hover(function(){
             if ($(this).children('.submenu-area').length == 1) {
@@ -21,37 +58,21 @@ $(function(){
             $('.gnb-area .submenu-area .link-submenu').removeClass('active');
             $('.gnb-area .submenu-item:first-child .link-submenu').addClass('active');
         }) // 소메뉴 hover
+        $(document).scroll(function(){
+            var scrollT = $(this).scrollTop(); 
+            var slideT = $(this).find('.sc-mainSlide').offset().top; 
+            var slideH = $(this).find('.sc-mainSlide').height();
+            var videoT = $(this).find('.sc-video').offset().top;
+            var videoH = $(this).find('.sc-video').innerHeight();
+        
+            if (scrollT >= videoT + videoH) {
+                $('.header').removeClass('fix') 
+            } else if (scrollT < slideT + slideH) {
+                $('.header').addClass('fix')
+            }
+        }) // 스크롤 이벤트
     }
-
-    $('.gnb-area .ic-menu').click(function(){
-        $(this).siblings('.open-menu-wrap').removeClass('hide');
-        $('body').addClass('none-scroll');
-
-        lol = gsap.timeline({duration: .1, delay: .5})
-        .to($('.lol1'), .3,{ className:"lol1" })
-        .to($('.lol2'), .3,{ className:"lol2" })
-        .to($('.lol3'), .3,{ className:"lol3" })
-        .to($('.lol3'), .3,{ className:"lol3 hide" })
-        .to($('.lol2'), .3,{ className:"lol2 hide" })
-        .to($('.lol1'), .3,{ className:"lol1 hide" })
-        .to($('.lol1'), .3,{ className:"lol1" })
-        .to($('.lol2'), .3,{ className:"lol2" })
-        .to($('.lol3'), .3,{ className:"lol3" })
-        .to($('.lol4'), .3,{ className:"lol4" })
-        .to($('.lol5'), .3,{ className:"lol5" })
-
-    })
     if (matchMedia("screen and (max-width: 1023px)").matches) {
-
-        $('.header .close-menu').click(function(e){
-            e.preventDefault();
-            $(this).parents('.open-menu-wrap').addClass('hide');
-            $('body').removeClass('none-scroll')
-            lol.kill();
-            $('.ani-lol span').each(function(){
-                $(this).addClass('hide');
-            })
-        }) // 메뉴 닫기 버튼 클릭시 메뉴 영역 닫고 body 스크롤 초기화
 
         $('.gnb-area .menu-area .has-sub .link-menu').click(function(e){
             e.preventDefault();
@@ -61,30 +82,17 @@ $(function(){
 
     var last_scroll = 0;
     $(window).scroll(function () {
-        if (!$('.header').hasClass('fix')) { // header 영역에 fix 클래스가 없을 때만 적용
+        if (!$('.header').hasClass('fix')) { 
             var curr_scroll = $(window).scrollTop();
-            if (curr_scroll > last_scroll || $(window).scrollTop() == 0) { // 마우스 내릴 때
+            if (curr_scroll > last_scroll || $(window).scrollTop() == 0) {
                 $('.header').addClass('hide');
-            } else { // 마우스 올릴 때
+            } else { 
                 $('.header').removeClass('hide');
             }
             last_scroll = curr_scroll;
         }
     }); // 마우스 휠 조작 시 헤더영역 숨김 유무
 
-    $(document).scroll(function(){
-        var scrollT = $(this).scrollTop(); // 현재 스크롤 위치
-        var slideT = $(this).find('.sc-mainSlide').offset().top; // Top에서 메인슬라이드 섹션 Top까지 위치
-        var slideH = $(this).find('.sc-mainSlide').height(); // 메인슬라이드 섹션 높이
-        var videoT = $(this).find('.sc-video').offset().top; // Top에서 비디오 섹션 Top까지 위치
-        var videoH = $(this).find('.sc-video').innerHeight(); // 비디오 섹션 높이(패딩포함)
-    
-        if (scrollT >= videoT + videoH) { // 현재 스크롤이 비디오 섹션 끝부분까지의 위치보다 크다면
-            $('.header').removeClass('fix') 
-        } else if (scrollT < slideT + slideH) { // 현재 스크롤이 메인 슬라이드 섹션 끝부분까지의 위치보다 작다면
-            $('.header').addClass('fix')
-        }
-    }) // 스크롤 이벤트
     
     
     
@@ -95,42 +103,41 @@ $(function(){
     } // 새로고침 시 스크롤 top
     
 
-    var numArray = []; // 5개의 난수를 넣어 줄 배열 생성
-    var slideArray = []; // 5개의 슬라이드를 넣어 줄 슬라이드 배열 생성
+    var numArray = [];
+    var slideArray = [];
     $('.sc-mainSlide .swiper-slide').each(function(){
-        var randomSlide = Math.floor(Math.random() * 17) + 1; // 슬라이드 17개에 맞춰 1~17까지 난수 값 설정
-        if(!numArray.includes(randomSlide)) { // 나온 난수 값이 배열 안에 없다면(중복 체크)
-            numArray.push(randomSlide); // 난수 값을 난수 배열에 넣어줌
-            slideArray.push($('.sc-mainSlide .swiper-slide:nth-child('+randomSlide+')').html()); // 나온 난수 값번째 슬라이드에 html을 슬라이드 배열에 넣어줌
+        var randomSlide = Math.floor(Math.random() * 17) + 1;
+        if(!numArray.includes(randomSlide)) { 
+            numArray.push(randomSlide);
+            slideArray.push($('.sc-mainSlide .swiper-slide:nth-child('+randomSlide+')').html());
         }
-        if(numArray.length == 5) { // 난수 배열의 길이가 5개가 된다면
-            $('.sc-mainSlide .swiper-wrapper').empty(); // 원래 있던 17개의 슬라이드를 모두 제거
+        if(numArray.length == 5) {
+            $('.sc-mainSlide .swiper-wrapper').empty();
             $(numArray).each(function(i){
-                $('.sc-mainSlide .swiper-wrapper').append('<div class="swiper-slide"></div>') // 새로운 슬라이드 영역 생성
-                $('.sc-mainSlide .swiper-slide:last-child').append(slideArray[i]); // 만든 슬라이드 영역에 슬라이드 배열에 저장해둔 html을 하나씩 넣어줌
+                $('.sc-mainSlide .swiper-wrapper').append('<div class="swiper-slide"></div>')
+                $('.sc-mainSlide .swiper-slide:last-child').append(slideArray[i]);
             })
         }
     }) // 메인슬라이드 개수를 5개로 제한하며 새로고침 할 때마다 랜덤슬라이드
     var isStop = false;
     function fadeText() {
-        $('.swiper-slide .fade').remove(); // 초기화
-        var text = $('.swiper-slide-active').find('.fiction').text(); // 현재 활성화된 슬라이드에 페이드 할 텍스트를 text 변수에 저장 
-        text = text.split(""); // 문장을 한 글자씩 잘라서 다시 text 변수에 저장
-        $('.swiper-slide-active').find('.desc').append('<span class="fade"></span>') // 활성화 된 슬라이드에 텍스트 자리 뒤에 fade 클래스를 가진 span을 추가
-        $(text).each(function(i){ // text의 길이만큼 반복문
-            $('.swiper-slide-active').find('.fade').append('<i class="fade-txt'+i+'">'+text[i]+'</i>'); // 추가한 fade안에 text변수에 저장된 글자 하나하나를 각각 클래스를 줘서 추가
+        $('.swiper-slide .fade').remove();
+        var text = $('.swiper-slide-active').find('.fiction').text().split("");
+        $('.swiper-slide-active').find('.desc').append('<span class="fade"></span>')
+        $(text).each(function(i){
+            $('.swiper-slide-active').find('.fade').append('<i class="fade-txt'+i+'">'+text[i]+'</i>');
         })
-        var i = 0; // setinterval 반복할 횟수 i를 0으로 초기화
-        var ani = setInterval(function(){
+        var i = 0;
+        var textAni = setInterval(function(){
             if(!isStop) {
-                if(i < text.length) { // text의 길이만큼 반복
-                    $('.swiper-slide-active').find('.fade-txt'+i+'').animate({opacity: "1"}); // 각각의 글자를 2초마다 opacity 1
+                if(i < text.length) {
+                    $('.swiper-slide-active').find('.fade-txt'+i+'').animate({opacity: "1"});
                     i++;
                 } else {
-                    return false; // setinterval 종료
+                    return false;
                 }
             } else {
-                clearInterval(ani);
+                clearInterval(textAni);
                 setTimeout(function(){
                     isStop = false;
                     fadeText();
